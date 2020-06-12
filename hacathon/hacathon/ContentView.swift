5f6479a469f295cc7f52f172f3bc5254802e9b3c
sai sathi <saisathi@sais-MacBook-Pro.local>
2020-06-12 19:21:30 -0400

    first commit

---
+++ hacathon/hacathon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hacathon
+//
+//  Created by sai sathi on 2020-06-05.
+//  Copyright © 2020 sai sathi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
